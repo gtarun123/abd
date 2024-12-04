7f5949e67c404bb2dcdffe3f6f749654ca9c848f
abdul-abd17 <aasmamr559@gmail.com>
2024-12-04 12:34:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,9 @@
-const button = document.getElementById("button");
-const toasts = document.getElementById("toasts");
+const password = document.getElementById("password");
+const background = document.getElementById("background");
 
-const messages = [
-  "Message One",
-  "Message Two",
-  "Message Three",
-  "Message Four",
-];
-const types = ["info", "success", "error"];
-
-const getRandomMessage = () =>
-  messages[Math.floor(Math.random() * messages.length)];
-
-const getRandomType = () => types[Math.floor(Math.random() * types.length)];
-
-const createNotification = (message = null, type = null) => {
-  const notif = document.createElement("div");
-  notif.classList.add("toast");
-  notif.classList.add(type ? type : getRandomType());
-  notif.innerText = message ? message : getRandomMessage();
-  toasts.appendChild(notif);
-  setTimeout(() => notif.remove(), 3000);
-};
-
-button.addEventListener("click", () => createNotification());
+password.addEventListener("input", (e) => {
+  const input = e.target.value;
+  const length = input.length;
+  const blurValue = 20 - length * 2;
+  background.style.filter = `blur(${blurValue}px)`;
+});
